1137fe31533155a0d6c1b3481b21f4d27bcefec7
Tony Wu <57581600@qq.com>
2019-05-31 16:39:34 +0800

    no test

--- pages/components/components/RptCampTrans.vue
+++ pages/components/components/RptCampTrans.vue
@@ -4,19 +4,19 @@
             <table class="ui selectable celled table">
                 <tr class="positive">
                     <th>市场活动</th>
-                    <th>产生例子数</th>
-                    <th>已经在跟进</th>
-                    <th>出席体验</th>
-                    <th>签约数量</th>
-                    <th>签约金额</th>
+                    <th class="center aligned">产生例子数</th>
+                    <th class="center aligned">已经在跟进</th>
+                    <th class="center aligned">出席体验</th>
+                    <th class="center aligned">签约数量</th>
+                    <th class="center aligned">签约金额</th>
                 </tr>
                 <tr v-for="s of stat">
-                    <td>{{s['campaign']}}</td>
-                    <td>{{s['产生例子数']}}</td>
-                    <td>{{s['已经在跟进']}}</td>
-                    <td>{{s['出席体验']}}</td>
-                    <td>{{s['签约数量']}}</td>
-                    <td>{{s['签约金额']}}</td>
+                    <td >{{s['campaign']}}</td>
+                    <td class="right aligned">{{s['产生例子数']}}</td>
+                    <td class="right aligned">{{s['已经在跟进']}}</td>
+                    <td class="right aligned">{{s['出席体验']}}</td>
+                    <td class="right aligned">{{s['签约数量']}}</td>
+                    <td class="right aligned">{{s['签约金额']}}</td>
                 </tr>
             <table>
         </div>
